93ebfd64329060ff03db3cab9c42c9e121ccd58f
paulo-martins182 <paulomn2013@hotmail.com>
2025-10-05 04:27:21 -0300

    feat: add dashboard store connection to api

--- app/api/store/is-seller/route.js
+++ app/api/store/is-seller/route.js
@@ -3,25 +3,22 @@ import authSeller from "@/middlewares/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(req){
-    try{
-        const { userId } = getAuth(req);
-        isSeller = await authSeller(userId)
+export async function GET(req) {
+  try {
+    const { userId } = getAuth(req);
+    const isSeller = await authSeller(userId);
 
-        if(!isSeller){
-            return NextResponse.json({message: "Unauthorized"}, { status: 401 });
-        }
-
-
-        const storeInfo = await prisma.store.findUnique({
-            where: {userId},
-        })
-
-
-        return NextResponse.json({isSeller, storeInfo}, { status: 200 });
-
-    }catch(e){
-        console.error("[STORE_IS_SELLER]", e);
-        return NextResponse.json({message: "Internal error"}, { status: 500 });
+    if (!isSeller) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
+
+    const storeInfo = await prisma.store.findUnique({
+      where: { userId },
+    });
+
+    return NextResponse.json({ isSeller, storeInfo }, { status: 200 });
+  } catch (e) {
+    console.error("[STORE_IS_SELLER]", e);
+    return NextResponse.json({ message: e.code || e.message }, { status: 400 });
+  }
 }
